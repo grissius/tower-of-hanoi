bb24e9af170fd93c3d60dd3c1d94a6eb146c7d03
Jaroslav Šmolík <grissius@gmail.com>
2017-10-11 08:45:48 +0200

    Add moving logic and interaction

--- tower.cpp
+++ tower.cpp
@@ -9,21 +9,54 @@ using namespace std;
 class Hanoi
 {
   public:
-    Hanoi() : discs(3)
+    Hanoi() : discs(3), startOn(1)
     {
         towers.reserve(3);
         for (int i = 0; i < 3; ++i)
         {
-            towers.push_back(new vector<int>());
+            vector<int>* vec = new vector<int>();
+            if(i == startOn) {
+                vec->push_back(3);
+                vec->push_back(2);
+                vec->push_back(1);
+            }
+            towers.push_back(vec);
         }
-        towers[0]->push_back(3);
-        towers[0]->push_back(2);
-        towers[0]->push_back(1);
-
-        towers[1]->push_back(2);
-        towers[1]->push_back(1);
-
-        towers[2]->push_back(1);
+    }
+    bool canMove(int from, int to) {
+        if(from < 0 || to < 0 || from >= towers.size() || to >= towers.size()) {
+            return false;
+        }
+        if((*towers[from]).size() <= 0) {
+            return false;
+        }
+        int disc = (*towers[from]).back();
+        if((*towers[to]).size() > 0) {
+            int discBellow = (*towers[to]).back();
+            if(disc >= discBellow) {
+                return false;
+            }
+        }
+        return true;
+    }
+    void move(int from, int to) {
+        if(canMove(from, to)) {
+            int disc = (*towers[from]).back();
+            (*towers[from]).pop_back();
+            (*towers[to]).push_back(disc);
+        }
+    }
+    bool isWon() {
+        if(!towers[startOn]->empty()) {
+            return false;
+        }
+        int nonEmptyTowers = 0;
+        for (vector<int> *tower : towers) {
+            if(!tower->empty()) {
+                nonEmptyTowers++;
+            }
+        }
+        return nonEmptyTowers == 1;
     }
     friend std::ostream &operator<<(ostream &os, const Hanoi &hanoi)
     {
@@ -70,10 +103,17 @@ class Hanoi
   private:
     vector<vector<int> *> towers;
     int discs;
+    int startOn;
 };
 
 int main(int argc, char **argv)
 {
     Hanoi hanoi;
-    cout << hanoi << endl;
+    int from, to;
+    cout << hanoi << endl;    
+    while(!hanoi.isWon()) {
+        cin >> from >> to;
+        hanoi.move(from-1, to-1);
+        cout << hanoi << endl;
+    }
 }
